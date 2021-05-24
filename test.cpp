839902bf5f88c7d3eaab6c4af173ea55804d0f7d
Abhishek Vaishnav <abhishek777vaishnav@gmail.com>
2021-05-24 13:24:05 +0530

    mathematics snippets added

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+#define MOD 1000000007
+/*
+   notes
+
+*/
+
+
+
+void solve()
+{
+  
+}
+
+
+int main() {
+  FAST
+  ll t = 1;
+  cin >> t;
+  while (t--)solve();
+  return 0;
+}
